ac0455c9fa2598eafca3c3e9eb9732f3f68e6088
alec-gironda <alec.gironda@gmail.com>
2022-01-02 23:51:04 -0500

    Initial Commit

---
+++ Beyond tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Beyond tvOS
+//
+//  Created by Alec Gironda on 1/2/22.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
